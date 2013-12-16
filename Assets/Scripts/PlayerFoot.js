fee180a3b4f2874aec627c80a5af011bf35c4de2
Neson <neson@dex.tw>
2013-12-16 17:00:31 +0800

    update gameobject tags

--- Assets/Scripts/PlayerFoot.js
+++ Assets/Scripts/PlayerFoot.js
@@ -10,13 +10,13 @@ function Update () {
 
 
 function OnTriggerEnter(what : Collider) {
-	if(what.gameObject.name == "Terrain" || what.gameObject.name == "wall" || what.gameObject.name == "house1" || what.gameObject.name == "house2" || what.gameObject.name == "house3" || what.gameObject.name == "house4" || what.gameObject.name == "house5") {
+	if (what.gameObject.tag == "Building" || what.gameObject.tag == "Wall" || what.gameObject.tag == "Ground") {
 		transform.parent.gameObject.GetComponent(PlayerControl).on_ground++;
 	}
 }
 
 function OnTriggerExit(what : Collider) {
-    if(what.gameObject.name == "Terrain" || what.gameObject.name == "wall" || what.gameObject.name == "house1" || what.gameObject.name == "house2" || what.gameObject.name == "house3" || what.gameObject.name == "house4" || what.gameObject.name == "house5") {
+    if (what.gameObject.tag == "Building" || what.gameObject.tag == "Wall" || what.gameObject.tag == "Ground") {
 		transform.parent.gameObject.GetComponent(PlayerControl).on_ground--;
 	}
 }
