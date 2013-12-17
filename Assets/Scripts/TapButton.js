1c33cb78ae57d219f221f8dbf6f7a678d9bb0d2a
Neson <neson@dex.tw>
2013-12-18 00:07:51 +0800

    update

--- Assets/Scripts/TapButton.js
+++ Assets/Scripts/TapButton.js
@@ -16,6 +16,7 @@
 #pragma strict
 
 @script RequireComponent( GUITexture )
+@script RequireComponent( AudioSource )
 
 static private var joysticks : Joystick[];  // A static collection of all joysticks
 static private var enumeratedJoysticks : boolean = false;
@@ -35,10 +36,24 @@ public var tapped : boolean;  // 點按
 public var held : boolean;  // 按住
 public var released : boolean;  // 放開
 
-public var press_animation = false;
+class Effects {
+	var sunkenWhenPressed = false;  // 按下後縮小
+	var sunkenRate = 0.88;  // 按下後縮小比例
+	var sunkenAnimation = false;  // 按下後縮小動畫
+	var sunkenAnimationInitialVelocity = 0.00;
+	var sunkenAnimationVelocityAcceleration = 0.02;
+	var sunkenAnimationMinScale = 0.86;
+	var sunkenAnimationMaxScale = 1.01;
+	var sunkenAnimationTime = 0.0;  // 按下後縮小動畫時間
+	var tapSFX : AudioClip;  // 點擊音效
+}
+public var effects : Effects;
 private var juststart = true;
-private var texture_center : Vector2;
+private var scale = 1.0;
 private var pixelInset_org : Rect;
+private var animation_step = 0;
+private var animation_next = 0;
+private var animation_v = 0f;
 
 function Start() {
 	// Cache this component at startup instead of looking up every frame
@@ -52,10 +67,13 @@ function Start() {
 	held = false;
 	released = false;
 
+	animation_v = effects.sunkenAnimationInitialVelocity;
 }
 
 
 function FixedUpdate() {
+
+	// Init
 
 	if (juststart) {
 		pixelInset_org = guiTexture.pixelInset;
@@ -63,6 +81,9 @@ function FixedUpdate() {
 	}
 
 	if (is_enabled) {
+
+		// Is pressed?
+
 		if ( !enumeratedJoysticks ) {
 			joysticks = FindObjectsOfType( Joystick ) as Joystick[];
 			enumeratedJoysticks = true;
@@ -100,24 +121,116 @@ function FixedUpdate() {
 			held = false;
 		}
 
+		// Texture
+
 		if (held && held_texture_in_use) {
 			guiTexture.texture = held_texture_in_use;
 		} else if (normal_texture_in_use) {
 			guiTexture.texture = normal_texture_in_use;
 		}
 
-	} else {
+	} else {  // disabled
 		if (disabled_texture_in_use) {
 			guiTexture.texture = disabled_texture_in_use;
 		}
 	}
 
-	if (press_animation) {
+	// Effects
+
+	if (effects.sunkenWhenPressed && !effects.sunkenAnimation) {
 		if (held) {
-			Scale(0.9);
+			Scale(effects.sunkenRate);
 		} else {
 			Scale(1);
 		}
+	} else if (effects.sunkenWhenPressed && effects.sunkenAnimation) {
+		if (tapped) {
+			animation_next = 1;
+		}
+		if (released) {
+			animation_next = 11;
+		}
+	}
+
+	if (animation_step == 0) {
+		if (animation_next == 1) {
+			animation_step = 1;
+			animation_next = 0;
+		}
+
+	} else if (animation_step == 1) {
+		if (scale+animation_v < (1-(1-effects.sunkenAnimationMinScale)/2) || scale > 1) {
+			animation_step = 2;
+		}
+		animation_v -= effects.sunkenAnimationVelocityAcceleration;
+		Scale(scale+animation_v);
+
+	} else if (animation_step == 2) {
+		if (scale+animation_v < effects.sunkenAnimationMinScale || scale > 1) {
+			animation_step = 3;
+		}
+		if (animation_v < effects.sunkenAnimationVelocityAcceleration) animation_v += effects.sunkenAnimationVelocityAcceleration;
+		Scale(scale+animation_v);
+
+	} else if (animation_step == 3) {
+		if (scale+animation_v > effects.sunkenRate) {
+			animation_step = 4;
+			Scale(effects.sunkenRate);
+			animation_v = effects.sunkenAnimationInitialVelocity;
+		}
+		animation_v += effects.sunkenAnimationVelocityAcceleration;
+		Scale(scale+animation_v);
+
+	} else if (animation_step == 4) {
+		if (animation_next == 11) {
+			animation_step = 11;
+			animation_next = 0;
+		}
+
+	} else if (animation_step == 11) {
+		if (animation_next == 1) {
+			animation_step = 1;
+			animation_next = 0;
+		}
+		if (scale+animation_v > (effects.sunkenRate-(effects.sunkenRate-effects.sunkenAnimationMaxScale)/2)) {
+			animation_step = 12;
+		}
+		animation_v += effects.sunkenAnimationVelocityAcceleration;
+		Scale(scale+animation_v);
+
+	} else if (animation_step == 12) {
+		if (animation_next == 1) {
+			animation_step = 1;
+			animation_next = 0;
+		}
+		if (scale+animation_v > effects.sunkenAnimationMaxScale) {
+			animation_step = 13;
+		}
+		if (animation_v > effects.sunkenAnimationVelocityAcceleration) animation_v -= effects.sunkenAnimationVelocityAcceleration;
+		Scale(scale+animation_v);
+
+	} else if (animation_step == 13) {
+		if (animation_next == 1) {
+			animation_step = 1;
+			animation_next = 0;
+		}
+		if (scale+animation_v < 1) {
+			animation_step = 0;
+			Scale(1);
+			animation_v = effects.sunkenAnimationInitialVelocity;
+		}
+		animation_v -= effects.sunkenAnimationVelocityAcceleration;
+		Scale(scale+animation_v);
+	}
+
+	if (effects.tapSFX && tapped) {
+		if (!GetComponent('AudioSource')) {
+			gameObject.AddComponent(AudioSource);
+			audio.dopplerLevel = 0;
+			audio.panLevel = 0;
+			audio.minDistance = Mathf.Infinity;
+		}
+		audio.PlayOneShot(effects.tapSFX);
 	}
 }
 
@@ -172,4 +285,6 @@ function Scale(p : float) {  // Scale the GUITexture to a specified percentage, 
 	var new_y = pixelInset_org.y-(new_height-pixelInset_org.height)/2;
 
 	guiTexture.pixelInset = new Rect(new_x, new_y, new_width, new_height);
+
+	scale = p;
 }
