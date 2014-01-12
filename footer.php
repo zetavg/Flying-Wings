264f80f8d5067f63acdba46aaca98dc38ca4a97f
Neson <neson@dex.tw>
2014-01-12 11:06:03 +0800

    add disqus

--- footer.php
+++ footer.php
@@ -18,8 +18,35 @@
     <!-- FOOTER -->
     <div class="footer-container">
       <footer class="container">
-        <p class="pull-right"><a href="#">Back to top</a></p>
-        <p>A [ NTUST Mobile Game Design ] Project. 此為向「進撃の巨人」高度致敬之作 (業)，絕無侵權之意。</p>
-        <p></p>
+        <p>　</p>
+        <p>　</p>
+        <div class="row">
+          <div class="col-lg-10">
+            <p>A [ NTUST Mobile Game Design ] Project. 此為向「進撃の巨人」高度致敬之作 (業)，絕無侵權之意。</p>
+          </div>
+          <div class="col-lg-2">
+            <p class="pull-right"><a href="#">Back to top</a></p>
+          </div>
+        <p>　</p>
+        </div>
       </footer>
+    </div>
+    <div class="footer-container footer-comments">
+      <div class="container comments">
+        <h2>Comments</h2>
+        <div id="disqus_thread"></div>
+        <script type="text/javascript">
+            /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+            var disqus_shortname = 'flyingwingsgame'; // required: replace example with your forum shortname
+
+            /* * * DON'T EDIT BELOW THIS LINE * * */
+            (function() {
+                var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+                dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+                (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+            })();
+        </script>
+        <noscript>Please enable JavaScript to view the <a href="http://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+        <a href="http://disqus.com" class="dsq-brlink">comments powered by <span class="logo-disqus">Disqus</span></a>
+      </div>
     </div>
