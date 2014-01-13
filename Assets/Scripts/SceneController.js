4223dbb9b27015ee0021cf7f523e094f3d76db23
Yukai <yukaihuang1993@hotmail.com>
2014-01-13 18:43:25 +0800

    complete scenecontroller.js basic structure

--- Assets/Scripts/SceneController.js
+++ Assets/Scripts/SceneController.js
@@ -14,7 +14,8 @@ public enum GameMode
 {
 	Defense, 
 	Killing, 
-	TimeAttack
+	TimeAttack,
+	Tutorial
 };
 
 public enum Level 
@@ -24,26 +25,26 @@ public enum Level
 	Insane
 };
 
-
+//關卡按鈕
 var defense : TapButton;
 var kill : TapButton;
 var timeattack : TapButton;
 var tutorial : TapButton;
+
+//關卡說明
+var defense_about : GUITexture;
+var kill_about : GUITexture;
+var timeattack_about : GUITexture;
+var tutorial_about : GUITexture;
+
+//兩個控制按鈕
 var back : TapButton;
-
 var play : TapButton;
-
-var kill_about : GameObject;
-var defense_about : GameObject;
-var timeattack_about : GameObject;
-var tutorial_about : GameObject;
 
 //難度設定按鈕
 var levelEasy : TapButton;
 var levelHard : TapButton;
 var levelInsane : TapButton;
-
-var controlButtons : GameObject[];
 
 //Set Default value for difficulty and gamemode
 var gameMode : GameMode = GameMode.Defense;
@@ -58,6 +59,7 @@ var TimeMd : TimeAttack;
 TimeMd = GameObject.Find("TimeAttack").GetComponent(TimeAttack);
 
 //var KillMd : Killing;
+//KillMd = GameObject.Find("Killing").GetComponent(Killing);
 
 function Awake()
 {
@@ -66,110 +68,105 @@ function Awake()
 
 
 function Start () {
-	InactivateAllContents();
 
 	//default choose EASY level
-	levelEasy.guiTexture.texture = levelEasy.held_texture[0];
+	InactivateLevelTextures();
+	levelEasy.UseSet(1);
+
+	//Inactivate Mode Contents with tag "Content"
+	InactivateAllContents();
 
 	//default play defense mode
-	defense_about.SetActive(true);
+	defense_about.gameObject.SetActive(true);
 }
 
 function Update () {
 
-	//各種壓住、推倒。
+	//遊戲按鈕行為：
+	{
+		if (defense.tapped || defense.held || defense.released) {
+			//button animation control
+			InactivateAllContents();
+			defense_about.gameObject.SetActive(true);
 
-	if (defense.tapped) {
-		//button animation control
-		InactivateAllContents();
-		defense_about.SetActive(true);
-
-		//啟用Defense模式
-		//InactivateAllModes();
-		//DefenseMd.activate = true;
-		gameMode = GameMode.Defense;
-	}
-
-	if (kill.tapped) {
-		//button animation control
-		InactivateAllContents();
-		kill_about.SetActive(true);
-
-		//啟用TimeAttack模式
-		//InactivateAllModes();
-		//TimeMd.activate = true;
-		gameMode = GameMode.Killing;
-	}
-
-	if (timeattack.tapped) {
-		InactivateAllContents();
-		timeattack_about.SetActive(true);
-
-		gameMode = GameMode.TimeAttack;
-	}
-
-	if (back.tapped) {
-		Destroy(gameObject);
-		Application.LoadLevel("Title");
-	}
-	
-	if (play.tapped) {
-		//Load Level Here and Setup up Objects According to activated mode
-		
-		//Application.LoadLevel("City");
-
-		switch(gameMode)
-		{
-			case GameMode.Defense:
-				DefenseMd.SetUpLevel(level);
-				DefenseMd.Activate();
-				break;
-
-			case GameMode.Killing:	
-				//,.......
-				break;	
-
-			case GameMode.TimeAttack:
-				TimeMd.SetUpLevel(level);
-				TimeMd.Activate();
-				break;
+			//啟用Defense模式
+			gameMode = GameMode.Defense;
 		}
 
+		if (kill.tapped || kill.held || kill.released) {
+			//button animation control
+			InactivateAllContents();
+			kill_about.gameObject.SetActive(true);
+
+			//啟用TimeAttack模式
+			gameMode = GameMode.Killing;
+		}
+
+		if (timeattack.tapped || timeattack.held || timeattack.released) {
+			InactivateAllContents();
+			timeattack_about.gameObject.SetActive(true);
+
+			gameMode = GameMode.TimeAttack;
+		}
+
+		if (tutorial.tapped || tutorial.held || tutorial.released) {
+			InactivateAllContents();
+			tutorial_about.gameObject.SetActive(true);
+
+			gameMode = GameMode.Tutorial;
+		}
+
+		if (back.tapped || back.held || back.released) {
+			Destroy(gameObject);
+			Application.LoadLevel("Title");
+		}
+		
+		if (play.tapped || play.held || play.released) {
+			//Load Level Here and Setup up Objects According to activated mode
+			
+			//Application.LoadLevel("City");
+			//或者load「載入中」畫面。
+
+			//用gameMode參數來啟動模式
+			switch(gameMode)
+			{
+				case GameMode.Defense:
+					DefenseMd.SetUpLevel(level);
+					DefenseMd.Activate();
+					break;
+
+				case GameMode.Killing:	
+					//,.......
+					break;	
+
+				case GameMode.TimeAttack:
+					TimeMd.SetUpLevel(level);
+					TimeMd.Activate();
+					break;
+			}
+
+		}
+		if (levelEasy.tapped || levelEasy.held || levelHard.released) 
+		{
+			level = Level.Easy;
+			InactivateLevelTextures();
+			levelEasy.UseSet(1);		
+			
+		}
+		if (levelHard.tapped || levelHard.held || levelHard.released)
+		{
+			level = Level.Hard;
+			InactivateLevelTextures();
+			levelHard.UseSet(1);
+
+		}
+		if (levelInsane.tapped || levelInsane.held || levelInsane.released)
+		{
+			level = Level.Insane;
+			InactivateLevelTextures();
+			levelInsane.UseSet(1);
+		}
 	}
-
-
-	if (levelEasy.tapped || levelEasy.released || levelEasy.held) 
-	{
-		print("Good");
-		InactiveLevelTextures();
-		level = Level.Easy;
-		levelEasy.UseSet(1);
-	}
-	if (levelHard.tapped || levelHard.released || levelHard.held)
-	{
-		InactiveLevelTextures();
-		level = Level.Hard;
-		levelHard.UseSet(1);
-	}
-	if (levelInsane.tapped || levelInsane.released || levelInsane.held)
-	{
-		InactiveLevelTextures();
-		level = Level.Insane;
-		levelInsane.UseSet(1);
-	}
-
-
-
-	controlButtons = GameObject.FindGameObjectsWithTag("ControlButton");
-
-	for (var each in controlButtons)
-	{
-		var tb = each.GetComponent(TapButton);
-		if (tb.released || !tb.held) tb.guiTexture.texture = tb.normal_texture[0];
-		if (tb.held) tb.guiTexture.texture = tb.held_texture[0];
-	}
-
-
 }
 
 function InactivateAllContents(){
@@ -181,14 +178,13 @@ function InactivateAllContents(){
 	}
 }
 
-function InactivateAllModes() {
-	DefenseMd.activate = false;
-	TimeMd.activate = false;
-	/* if there are other modes write here */
-}
 
-function InactiveLevelTextures() {
+function InactivateLevelTextures() {
 	levelEasy.UseSet(0);
 	levelHard.UseSet(0);
 	levelInsane.UseSet(0);
+}
+
+function ByeeeeMi() {
+	Destroy(gameObject);
 }
